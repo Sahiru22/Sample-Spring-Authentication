a2a1ae06573d3dd2354e6ef5505764489999332c
Sahiru22 <sahirubalasuriya222@gmail.com>
2024-09-20 11:42:26 +0530

    first commit

---
+++ src/main/java/com/example/demo_spring_authentication/DemoSpringAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_spring_authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringAuthenticationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo_spring_authentication/DemoSpringAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_spring_authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
